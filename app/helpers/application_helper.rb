4fce6fa986f3104945f99cee5aa38e0edd299858
Yuta <ymmt7974@gmail.com>
2020-10-04 17:07:04 +0900

    静的ページ追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    # ページごとの完全なタイトルを返す
+    def full_title(page_title = '')
+      base_title = "Chores List"
+      if page_title.empty? 
+        base_title
+      else 
+        base_title + " | " +  page_title
+      end
+    end
+
 end
