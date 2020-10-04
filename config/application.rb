ad8a5531ef122cfc021e60fab821f02ee47f4acd
Yuta <ymmt7974@gmail.com>
2020-10-05 01:49:03 +0900

    日本語対応/タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,12 @@ module ChoresList
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # 表示TimeZone
+    config.time_zone = 'Asia/Tokyo'
+    # DB保存時間をlocal(Tokyo)にする
+    config.active_record.default_timezone = :local
+
+    # i18n
+    config.i18n.default_locale = :ja
   end
 end
